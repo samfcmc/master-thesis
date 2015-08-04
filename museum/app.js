745e9b90e4fc5e1aafb8ce9d0228b755f5b8c718
Samuel Coelho <samuelfcmc@gmail.com>
2015-08-04 14:50:29 +0100

    Moved musem app to another repo

--- museum/app.js
+++
@@ -1,8 +0,0 @@
-var app = require('./server');
-
-var port = process.env.PORT || 9000;
-
-app.listen(port, function() {
-  console.log('App up and running');
-  console.log('Listening on port ' + port);
-});
--- museum/src/js/vendors.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-(function(window, require) {
-  window.jQuery = require('jquery');
-  require('bootstrap');
-  require('angular');
-  require('angular-ui-router');
-  require('angular-resource');
-}(window, require));
