8bdf6d8c7e795e112dd9cc4f45356ba9c5c238dd
Samuel Coelho <samuelfcmc@gmail.com>
2015-09-04 10:39:44 +0100

    Using minted package for code snippets. Added list of listings to the table of contents

--- dissertation/clean.sh
+++ dissertation/clean.sh
@@ -1,4 +1,4 @@
-FILES="*.aux .bb *.bl *.lo *.lo *.nl *.ou *.to *.bc *.run.xml *.bbl *.blg *.fdb_latexmk *.fls *.log *.toc *.synctex.gz *.acn *.glo *.ist *.lof *.lot *.out *.acr *.alg *.glg *.gls *.glsdefs"
+FILES="*.aux .bb *.bl *.lo *.lo *.nl *.ou *.to *.bc *.run.xml *.bbl *.blg *.fdb_latexmk *.fls *.log *.toc *.synctex.gz *.acn *.glo *.ist *.lof *.lot *.out *.acr *.alg *.glg *.gls *.glsdefs *.lol"
 rm -f $FILES
 cd abstract
 rm -f $FILES
