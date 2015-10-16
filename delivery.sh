a0ca5c021c8ae88f1e68fc09e2c43ef163abaad8
Samuel Coelho <samuelfcmc@gmail.com>
2015-10-16 18:35:14 +0100

    Improved delivery script

--- delivery.sh
+++ delivery.sh
@@ -1,13 +1,17 @@
 FILENAME=MEIC-69350-Samuel-Coelho.zip
+rm -f $FILENAME
 echo "Compiling dissertation"
 cd dissertation
 make clean
 make
+cp dissertation.pdf ..
 cd ..
 echo "Compiling article"
 cd article
 make clean
 make
+cp article.pdf ..
 cd ..
 echo "Generating final zip file $FILENAME"
-zip $FILENAME dissertation/dissertation.pdf article/article.pdf
+zip $FILENAME dissertation.pdf article.pdf
+rm dissertation.pdf article.pdf
